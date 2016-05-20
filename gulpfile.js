54888244384331ec54eefa14eb250dc8bd33ffb6
James Merrill <James Merrill>
2016-05-20 17:49:56 -0400

    added homepage breakup

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var imageResize = require('gulp-image-resize');
 var autoprefixer = require('gulp-autoprefixer');
+var plumber = require('gulp-plumber');
 var browserSync = require('browser-sync').create();
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
 
     browserSync.init({
-        proxy: "localhost"
+        proxy: "localhost:3000"
     });
 
     gulp.watch("assets/scss/**/*.scss", ['sass']);
@@ -18,11 +20,20 @@ gulp.task('sass', function() {
     return gulp.src("assets/scss/*.scss")
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
-			browsers: ['last 3 versions'],
-			cascade: false
-		}))
+            browsers: ['last 3 versions'],
+            cascade: false
+        }))
         .pipe(gulp.dest("assets/css/dist"))
         .pipe(browserSync.stream());
+});
+
+gulp.task('resize', function() {
+    return gulp.src('assets/art/full/*.jpg')
+        .pipe(plumber())
+        .pipe(imageResize({
+            width: 250
+        }))
+        .pipe(gulp.dest('assets/art/thumb'));
 });
 
 gulp.task('default', ['serve']);
