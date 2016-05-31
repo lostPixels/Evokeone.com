cbc5175e4b771998bd5dac27c78a2034661abffe
James Merrill <James Merrill>
2016-05-31 17:46:45 -0400

    dicking around with memories

--- models/memories.js
+++ models/memories.js
@@ -1,5 +1,21 @@
 function get(){
-    return 'hi';
+    return [
+        {
+            name:'testing 123',
+            date: 'march 1, 2017',
+            comments:  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in volupt'
+        },
+        {
+            name:'testing 3456',
+            date: 'march 1, 2017',
+            comments:  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in volupt'
+        },
+        {
+            name:'testing 6789',
+            date: 'march 1, 2017',
+            comments:  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in volupt'
+        }
+    ]
 }
 
 function save(){
