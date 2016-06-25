8519718fbfd29787262a471dbf4f19f214539140
teddywhy <davidteoman@gmail.com>
2016-06-24 17:43:46 -0700

    Got CouchDB working to initialize. Using Memcached.

--- models/memories.js
+++ models/memories.js
@@ -1,27 +1,105 @@
+// NOTE: memcached must be installed or this won't work :(
+
+const
+    q = require('q'),
+    CouchDB = require('node-couchdb'),
+    MemcacheNode = require('node-couchdb-plugin-memcached'),
+    couch = new CouchDB({
+    cache: new MemcacheNode(),
+    auth: {
+        user: 'evoke',
+        pass: 'vkdoer591'
+    }
+});
+
+
 function get(){
-    return [
-        {
-            name:'testing 123',
-            date: 'march 1, 2017',
-            comments:  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in volupt'
-        },
-        {
-            name:'testing 3456',
-            date: 'march 1, 2017',
-            comments:  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in volupt'
-        },
-        {
-            name:'testing 6789',
-            date: 'march 1, 2017',
-            comments:  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in volupt'
-        }
-    ]
+    return {
+        memories:
+                [
+                    {
+                        name:'testing 123',
+                        date: 'march 1, 2017',
+                        comments:  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in volupt'
+                    },
+                    {
+                        name:'testing 3456',
+                        date: 'march 1, 2017',
+                        comments:  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in volupt'
+                    },
+                    {
+                        name:'testing 6789',
+                        date: 'march 1, 2017',
+                        comments:  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in volupt'
+                    }
+                ]
+    };
 }
+
+/*
+
+    TODO:
+    1. INTEGRATE COUCHDB
+    2. MAKE LAYOUT
+    3. INTEGRATE CAPTCHA
+
+*/
 
 function save(){
 
 }
 
+// this is ugly as balls
+// look into way to make this prettier
+
+// install Q library :)
+function init(){
+    console.log("I GOT CALLED");
+    couch.dropDatabase('evoke-memories').then(
+        function(){
+            console.log("DROPPED DATABASE");
+            couch.createDatabase('evoke-memories').then(
+                function(){
+                    console.log("CREATED DATABASE");
+
+                    // HERE IS WHERE IT BREAKS
+                    couch.uniqid().then(
+                        function(ids){
+                            console.log("GOT ID " + ids[0]);
+                            couch.insert("evoke-memories", {
+                                _id: ids[0],
+                                name: 'Testing 1337',
+                                date: 'June 23, 2016',
+                                comments: 'Yo yo yo couchDB in the hizzy.'
+                            }).then(
+                                function(response){
+                                    console.log("DATA: " + response.data + " STATUS " + response.status);
+                                    return response;
+                                },
+                                function(err){
+                                    console.log("JESUS CHRIST I GOT IN THIS FAR AND YOU STILL GAVE ME AN ERROR");
+                                    console.log("FUCK YOU NODE. Here's the error: ");
+                                    console.log(err);
+                                }
+                            )
+                        },
+                        function(err){
+                            console.log("SOMETHING FUCKED UP");
+                            console.log(err);
+                        }
+                    )
+                },
+                function(err){
+                    console.log("Error creating database " + err);
+                }
+            )
+        },
+        function(err){
+            console.log("Error dropping database " + err);
+    });
+}
+
 
 
 exports.get = get;
+exports.init = init;
--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 var express = require('express'),
     exphbs = require('express-handlebars'),
     routes = require('./routes'),
+    handlebars = require('handlebars'),
+    help = require('handlebars-form-helpers'),
     bodyParser = require('body-parser');
+
+// THIS WAS NOT STRAIGHTFORWARD AT ALL
+help.register(handlebars);
 
 var app = express();
 
-app.engine('handlebars', exphbs({
+var hbs = exphbs.create({
+	helpers: help.helpers,
     defaultLayout: 'main'
-}));
+});
+
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(routes);
