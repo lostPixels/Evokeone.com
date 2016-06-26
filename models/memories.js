3c53795cc288366cc371364faebf057fac629f91
teddywhy <davidteoman@gmail.com>
2016-06-26 15:12:32 -0700

    Wrote flexbox code for memories.

--- models/memories.js
+++ models/memories.js
@@ -14,18 +14,21 @@ function get(){
         memories:
                 [
                     {
-                        name:'testing 123',
+                        title:'I love Evoke',
+                        name:'James',
                         date: 'march 1, 2017',
                         comments:  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in volupt'
                     },
                     {
-                        name:'testing 3456',
+                        title: 'Evoke rocks!',
+                        name:'Ted',
                         date: 'march 1, 2017',
                         comments:  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in volupt'
                     },
                     {
-                        name:'testing 6789',
-                        date: 'march 1, 2017',
+                        title: 'GG NOOBS',
+                        name:'DepthCore',
+                        date: 'march 20, 2017',
                         comments:  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in volupt'
                     }
                 ]
