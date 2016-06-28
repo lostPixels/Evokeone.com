c7f4b47848c24ce4f632b470a9d26eee86f9c028
TeomanDavid <davidteoman@gmail.com>
2016-06-27 17:14:34 -0700

    CouchDB List Function working

--- assets/js/memory.js
+++ assets/js/memory.js
@@ -3,7 +3,7 @@
 templates['memory'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "<figure class=\"memory\">\n  <h2>"
+  return "<figure class=\"memory\" style=\"display:none\">\n  <h2>"
     + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
     + "</h2>\n  <span>"
     + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
