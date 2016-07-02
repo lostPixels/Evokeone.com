99e4814a485fb7aea74bf7ef3ab1692a6320cbb6
TeomanDavid <davidteoman@gmail.com>
2016-07-02 01:28:16 -0700

    Finished layout for interview page.

--- models/interviews.js
+++ models/interviews.js
@@ -1,9 +1,28 @@
 // again, load dynamically in future
 // mock = mock data
-var interviewData = require('../data/interview/forever-mock.json');
+var interviewData = require('../data/interview/forever.json');
 
 function get(id){
+  interviewData = preprocess(interviewData);
   return interviewData;
+}
+
+function preprocess(data){
+  var initialVars = ['interviewer1', 'interviewer2', 'artist'];
+  initialVars.forEach(function(v){
+      if(data[v]) {
+        data[v + "-initials"] = initials(data[v]);
+      }
+  });
+  return data;
+}
+
+function initials(s){
+  var output = "";
+  s.split(" ").forEach(function(name){
+    output += name.charAt(0).toUpperCase();
+  });
+  return output;
 }
 
 module.exports.get = get;
